3825ae854b5852765fce25ce399637140fce808f
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2026-03-08 10:31:00 +0100

    Working example celery.

--- api/celery_app.py
+++ api/celery_app.py
@@ -1,7 +1,13 @@
+import os
+
 from celery import Celery
+
+
+broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+result_backend = os.getenv("CELERY_RESULT_BACKEND", broker_url)
 
 celery = Celery(
     "app",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker=broker_url,
+    backend=result_backend,
 )
